5b21fd05e43680b02f46a862cd2373c18f08dd7b
Asher <124173983+99lash@users.noreply.github.com>
2025-09-25 00:38:46 +0800

    feat: implemented session manager and added /guide routes

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+App\Security\SessionManager::start();
+
 $router = new \App\Config\Router;
-
-
 /** 
  *  @method GET
  *  @route  /
@@ -12,14 +12,14 @@ $router->get('/', 'HomeController@index');
 
 /** 
  * @group /accounts 
-*/
+ */
 
 require_once __DIR__ . './assetRoutes.php';
 
 /** 
  * @group /summary 
  * 
-*/
+ */
 
 require_once __DIR__ . './summaryRoutes.php';
 
@@ -33,3 +33,10 @@ require_once __DIR__ . './authRoutes.php';
  * @group /users 
  */
 require_once __DIR__ . './userRoutes.php';
+
+/** 
+ *  @method GET
+ *  @route  /guide
+ *  @desc   Render guide page
+ */
+$router->get('/guide', 'GuideController@index');

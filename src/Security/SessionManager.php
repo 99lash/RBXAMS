cf4b6903ad783ca461fc079fc9061423d8d0a98b
Asher <124173983+99lash@users.noreply.github.com>
2025-09-25 00:39:37 +0800

    feat: added SessionManager class

---
+++ src/Security/SessionManager.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Security;
+
+class SessionManager
+{
+  public static function start()
+  {
+    ini_set('session.cookie_httponly', 1);
+    ini_set('session.use_only_cookies', 1);
+    ini_set('session.cookie_secure', isset($_SERVER['HTTPS']));
+    ini_set('session.cookie_samesite', 'Strict');
+
+    if (session_status() === PHP_SESSION_NONE) {
+      session_start();
+    }
+
+    $timeout = $_ENV['TIMEOUT_DURATION'] ?? 10;
+    // echo $timeout;
+    if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > intval($timeout))) {
+      self::destroy();
+    }
+    $_SESSION['LAST_ACTIVITY'] = time();
+  }
+
+  public static function regenerate()
+  {
+    session_regenerate_id(true);
+  }
+
+  public static function destroy()
+  {
+    $_SESSION = [];
+    if (ini_get("session.use_cookies")) {
+      $params = session_get_cookie_params();
+      setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+      );
+    }
+    session_destroy();
+  }
+}

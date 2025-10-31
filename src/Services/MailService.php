2df124c7df102bc6e13f9392795da32797b4f753
Asher <124173983+99lash@users.noreply.github.com>
2025-10-31 12:38:16 +0800

    fix(mail): update SMTP username to use environment variable

--- src/Services/MailService.php
+++ src/Services/MailService.php
@@ -16,8 +16,8 @@ class MailService
       $mail->isSMTP();                                            // Send using SMTP
       $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
       $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-      $mail->Username   = 'asherjohn48@gmail.com';                     // SMTP username
-      $mail->Password   = $_ENV['APP_PASSWORD'];                               // SMTP password
+      $mail->Username   = $_ENV['APP_EMAIL'];                     // SMTP username
+      $mail->Password   = $_ENV['APP_PASSWORD'];                  // SMTP password
       $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
       $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
